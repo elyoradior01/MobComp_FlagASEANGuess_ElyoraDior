a4d81c0b31b24a482cd611731b5c986914144bdc
Elyora Dior <elyoradior01@stduent.ciputra.ac.id>
2023-10-14 23:29:52 +0700

    Initial Commit

---
+++ MobComp_GameBendera_ElyoraDior/MobComp_GameBendera_ElyoraDiorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobComp_GameBendera_ElyoraDiorApp.swift
+//  MobComp_GameBendera_ElyoraDior
+//
+//  Created by MacBook Pro on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MobComp_GameBendera_ElyoraDiorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
